68f0ea101c731cc8df237258418fb3de2e7ce362
FrostN0v0 <1614591760@qq.com>
2022-04-29 17:29:52 +0800

    test114514

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'HMCL-PE',
+  title: 'HMCL-PE-Docs',
   tagline: 'Hello Minecraft! Launcher For Android',
   url: 'https://hmclpe.adorable0v0.top',
   baseUrl: '/',
