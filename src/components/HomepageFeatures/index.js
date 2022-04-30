ca34a31189b44a681e07110ab7974288d5af1285
FrostN0v0 <1614591760@qq.com>
2022-05-01 02:56:57 +0800

    Music

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: '帮助文档',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -14,7 +14,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '专注于MC服务器',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
